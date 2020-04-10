14f79f8e2b4ff9d30a7131e2e897627841add091
SohanR <mizan.rahman66d@gmail.com>
2020-04-10 23:30:27 +0600

    year to cent

---
+++ Arcade/Intro/The Journey Begins/CPlusPlus/CenturyFromYear.cpp
@@ -0,0 +1,8 @@
+//https://app.codesignal.com/arcade/intro/level-1/egbueTZRRL5Mm4TXN
+
+//Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+
+int centuryFromYear(int year)
+{
+    return ((year + 99) / 100);
+}
