cd2fa5045a8947f695c020d43b598049927af13d
SohanR <mizan.rahman66d@gmail.com>
2020-04-10 22:37:44 +0600

    add number

---
+++ Arcade/Intro/The Journey Begins/CPlusPlus/add.cpp
@@ -0,0 +1,8 @@
+//https://app.codesignal.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa/drafts
+
+//Write a function that returns the sum of two numbers.
+
+int add(int param1, int param2)
+{
+    return param1 + param2;
+}
