5d89f50d0a21d46b911480c4ac7f08a377e37149
SohanR <mizan.rahman66d@gmail.com>
2020-04-14 02:35:29 +0600

    cent ot year

---
+++ Arcade/Intro/The Journey Begins/JavaScript/centuryFromeYear.js
@@ -0,0 +1,5 @@
+//Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+
+function centuryFromYear(year) {
+  return Math.ceil(year / 100);
+}
