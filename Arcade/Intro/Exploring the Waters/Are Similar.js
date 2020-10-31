09199a33408d8db62123a8f885a88b1ad6794a70
sohanr <mizan.rahman66d@gmail.com>
2020-10-31 18:01:40 +0600

    are similar

---
+++ Arcade/Intro/Exploring the Waters/Are Similar.js
@@ -0,0 +1,24 @@
+//https://app.codesignal.com/arcade/intro/level-4/xYXfzQmnhBvEKJwXP/
+
+function areSimilar(a, b) {
+    if(a.toString()  == b.toString()){
+        return true;
+    }
+    
+    let c= [];
+    let d = [];
+    
+    for(let i = 0; i < a.length; i++){
+        if( a[i] != b[i]){
+            c.push(a[i])
+            d.push(b[i])
+        }
+    }
+    
+    d.reverse()
+    
+    if(c.length === 2 & c.toString() == d.toString()){
+        return true
+    }
+    return false;
+}
