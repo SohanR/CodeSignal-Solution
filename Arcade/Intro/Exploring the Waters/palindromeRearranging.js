12ec0d5f4d4ee327916d67bdb835d9964ba48f93
sohanr <mizan.rahman66d@gmail.com>
2020-11-02 20:56:56 +0600

    function palindromeRearranging(inputString) {

---
+++ Arcade/Intro/Exploring the Waters/palindromeRearranging.js
@@ -0,0 +1,23 @@
+function palindromeRearranging(inputString) {
+    const charArray  = inputString.split("");
+    
+    let letterObj = {};
+    
+    for(let i = 0; i < inputString.length; i++){
+        if(!letterObj.hasOwnProperty(charArray[i])){
+            letterObj[charArray[i]] = 1;
+        }else{
+            letterObj[charArray[i]]++;
+        }
+    }
+    
+    
+    let oddCount =0;
+    for(val in letterObj){
+        if(letterObj[val] % 2 != 0){
+            oddCount++
+        }
+    }
+    
+    return oddCount > 1 ? false : true;
+}
